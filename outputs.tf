19da0787bcf445788508f23941b67f4ce8057ac2
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:46 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "managed_redis_access_policy_assignments" {
-  description = "All managed_redis_access_policy_assignment resources"
-  value       = azurerm_managed_redis_access_policy_assignment.managed_redis_access_policy_assignments
-}
 output "managed_redis_access_policy_assignments_managed_redis_id" {
-  description = "List of managed_redis_id values across all managed_redis_access_policy_assignments"
-  value       = [for k, v in azurerm_managed_redis_access_policy_assignment.managed_redis_access_policy_assignments : v.managed_redis_id]
+  description = "Map of managed_redis_id values across all managed_redis_access_policy_assignments, keyed the same as var.managed_redis_access_policy_assignments"
+  value       = { for k, v in azurerm_managed_redis_access_policy_assignment.managed_redis_access_policy_assignments : k => v.managed_redis_id }
 }
 output "managed_redis_access_policy_assignments_object_id" {
-  description = "List of object_id values across all managed_redis_access_policy_assignments"
-  value       = [for k, v in azurerm_managed_redis_access_policy_assignment.managed_redis_access_policy_assignments : v.object_id]
+  description = "Map of object_id values across all managed_redis_access_policy_assignments, keyed the same as var.managed_redis_access_policy_assignments"
+  value       = { for k, v in azurerm_managed_redis_access_policy_assignment.managed_redis_access_policy_assignments : k => v.object_id }
 }
 
