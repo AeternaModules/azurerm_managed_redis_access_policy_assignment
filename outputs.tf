e3592c29bfa84573b36d21417427711bb3f8a8ce
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:03:15 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "managed_redis_access_policy_assignments_id" {
+  description = "Map of id values across all managed_redis_access_policy_assignments, keyed the same as var.managed_redis_access_policy_assignments"
+  value       = { for k, v in azurerm_managed_redis_access_policy_assignment.managed_redis_access_policy_assignments : k => v.id }
+}
 output "managed_redis_access_policy_assignments_managed_redis_id" {
   description = "Map of managed_redis_id values across all managed_redis_access_policy_assignments, keyed the same as var.managed_redis_access_policy_assignments"
   value       = { for k, v in azurerm_managed_redis_access_policy_assignment.managed_redis_access_policy_assignments : k => v.managed_redis_id }
